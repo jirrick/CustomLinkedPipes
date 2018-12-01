e261fbd70fc319dc16451cc22fe41c674306dada
jirrick <jurahudec@gmail.com>
2018-12-01 20:55:42 +0100

    revert

--- custom-start.sh
+++ custom-start.sh
@@ -3,9 +3,6 @@
 cd /etl
 exec ./start.sh &
 
-#spustit nginx
-nginx -g 'daemon off;'
-
 until curl -s --head  --request GET http://localhost:8080 | grep "200 OK" > /dev/null; do 
     sleep 1s
     echo "waiting for LinkedPipes"
@@ -13,3 +10,6 @@ done
 
 #spustit pipeline ze souboru
 curl -X POST -H "Content-Type: multipart/form-data" -F "pipeline=@docker-pipeline.jsonld" localhost:8080/resources/executions
+
+#spustit nginx
+nginx -g 'daemon off;'
