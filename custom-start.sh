3a41c79b7d2256c6ffaabd275c4e91ca16a68d6c
jirrick <jurahudec@gmail.com>
2018-12-01 20:45:05 +0100

    fix statistik, spustit nginx hned

--- custom-start.sh
+++ custom-start.sh
@@ -3,6 +3,9 @@
 cd /etl
 exec ./start.sh &
 
+#spustit nginx
+nginx -g 'daemon off;'
+
 until curl -s --head  --request GET http://localhost:8080 | grep "200 OK" > /dev/null; do 
     sleep 1s
     echo "waiting for LinkedPipes"
@@ -10,6 +13,3 @@ done
 
 #spustit pipeline ze souboru
 curl -X POST -H "Content-Type: multipart/form-data" -F "pipeline=@docker-pipeline.jsonld" localhost:8080/resources/executions
-
-#spustit nginx
-nginx -g 'daemon off;'
